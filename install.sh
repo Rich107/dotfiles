84b0ba8068fd0866476f61a5315494e2c05535e4
richard_ellison <small.car3251@fastmail.com>
2025-03-14 18:06:47 +0000

    starting to use this in anger and test

--- install.sh
+++ install.sh
@@ -1,43 +1,31 @@
 #!/bin/bash
 
-# Set up XDG_CONFIG_HOME
-export XDG_CONFIG_HOME="$HOME"/.config
-mkdir -p "$XDG_CONFIG_HOME"
-mkdir -p "$XDG_CONFIG_HOME"/nixpkgs
+# This was origainlly copied from a nix base but that is not currently working below is what we need to ensure is installed:
+# There was then an alternative for Brew but that will only work on a linux machine
+# Brew does not work inside a container running on a M chip Mac
+# packages=(
+#     fd
+#     ripgrep
+#     npm
+#     lazygit
+#     fzf
+#     kubectl
+#     direnv
+# )
 
-# wip need to pull in my .zsh config
-# curl -o "$HOME/.zshrc" https://raw.githubusercontent.com/mischavandenburg/dotfiles/main/.zshrc
-# curl -o "$HOME/.zprofile" https://raw.githubusercontent.com/mischavandenburg/dotfiles/main/.zprofile
-
-# Nvim should already be installed
-# curl -o "$HOME/.config/nvim/" https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz && rm -rf /opt/nvim && tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-
-packages=(
-    fd
-    ripgrep
-    npm
-    lazygit
-    fzf
-    kubectl
-    direnv
-)
-
-# Iterate over the array and install each package
-for package in "${packages[@]}"; do
-    echo "Installing $package..."
-    /home/linuxbrew/.linuxbrew/bin/brew install "$package"
-done
+# lazygit:
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
 
 echo "All packages have been installed."
 
 # Set up pure prompt
+git clone https://github.com/sindresorhus/pure.git "~/.zsh/pure"
 
-mkdir -p "$HOME/.zsh"
-git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
-
-# Silence source warning
-
-touch "$HOME/.privaterc"
+# Nvim config:
+git clone https://github.com/Rich107/neovim-config.git ~/.config/nvim/
 
 # Set up completions
 
