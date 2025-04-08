eb41a1799a0eb89dce1e60d56d88d24725566f7f
Rich107 <small.car3251@fastmail.com>
2025-04-08 22:20:06 +0100

    removed unwanted comments

--- install.sh
+++ install.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# This was origainlly copied from a nix base but that is not currently working below is what we need to ensure is installed:
-# There was then an alternative for Brew but that will only work on a linux machine
-# Brew does not work inside a container running on a M chip Mac
-# packages=(
-#     fd
-#     ripgrep
-#     npm
-#     lazygit
-#     fzf
-#     kubectl
-#     direnv
-# )
-
-export IN_CONTAINER=true
-
 echo "Installing lazygit:"
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
