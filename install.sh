c32efbb378ab9a4a345250fa591d18ceacc701fd
Rich107 <small.car3251@fastmail.com>
2025-05-29 12:35:14 +0100

    added gh and gh-dash
    gh-dash seems to need login first

--- install.sh
+++ install.sh
@@ -62,6 +62,19 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Install NVM for npm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+echo "Install GitHub CLI and TUI"
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
+    sudo mkdir -p -m 755 /etc/apt/keyrings &&
+    out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg &&
+    cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+    sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+    sudo apt update &&
+    sudo apt install gh -y
+apt update
+apt install gh
+gh extension install dlvhdr/gh-dash
+
 echo "Aliases:"
 echo "alias ls='eza -lh'" >>~/.zshrc
 
